181b8cde5556681d1747940ae0f8d978dd39fdcc
Roman <manko.roman1123@vu.cdu.edu.ua>
2024-12-19 14:53:06 +0200

    class figure

---
+++ Figure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+abstract class Figure
+{
+    protected int CenterX { get; set; }
+    protected int CenterY { get; set; }
+
+    public Figure(int x, int y)
+    {
+        CenterX = x;
+        CenterY = y;
+    }
+
+    public abstract void DrawBlack(Graphics g);
+    public abstract void HideDrawingBackGround(Graphics g, Color backgroundColor);
+    //
+    
+}
