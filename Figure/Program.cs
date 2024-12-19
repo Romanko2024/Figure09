ef1178d7a4bc9b71def5135ef4288989f13d71ad
Roman <manko.roman1123@vu.cdu.edu.ua>
2024-12-19 15:12:09 +0200

    MoveRight

--- Figure/Program.cs
+++ Figure/Program.cs
@@ -14,5 +14,15 @@ abstract class Figure
     public abstract void DrawBlack(Graphics g);
     public abstract void HideDrawingBackGround(Graphics g, Color backgroundColor);
     //
-    
+    public void MoveRight(Graphics g, Color backgroundColor, int distance, int step = 10)
+    {
+        for (int i = 0; i < distance; i += step)
+        {
+            DrawBlack(g); //намалювати
+            Thread.Sleep(100); //зачекати
+            HideDrawingBackGround(g, backgroundColor); //стерти
+            CenterX += step; //змінити координату
+        }
+    }
+
 }
